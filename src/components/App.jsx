0bfa9ee94e14db1286b3011d9b14df6ac45c39e7
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-07-31 13:15:43 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component} from 'react'
+
+export class App extends Component {
+  state = {
+    filter: ''
+  }
+
+  render() {
+      return (
+  console.log()
   );
+  }
+  
 };
